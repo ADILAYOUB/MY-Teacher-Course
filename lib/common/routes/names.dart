c1450fe7ac7ff5483617d9d8c7c83fa038b3767f
ADILAYOUB <mintofree840@gmail.com>
2023-09-06 13:08:29 +0530

    names of route

---
+++ lib/common/routes/names.dart
@@ -0,0 +1,10 @@
+class AppRoutes {
+  // welcome or on boarding screen
+  static const String INITIAL = '/';
+  // home page
+  static const String HOME = '/home';
+  // sign in screen
+  static const String LOGIN = '/sign_in';
+  // registration screen
+  static const String REGISTER = '/register';
+}
